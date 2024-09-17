a1ea07cd0a9f3994919110fb2bec0dcf83ab0950
khushbooattrish@gmail.com <dk5265534@gmail.com>
2024-09-17 14:43:39 +0530

    upload first version

---
+++ models/Student.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const StudentSchema=new Schema({
+    rollNo:{type:Number},
+    name:{type:String},
+    fatherName:{type:String},
+    adharcardNumber:{type:String},
+    moblieNumber:{type:String}
+})
+
+    module.exports=mongoose.model('student', StudentSchema)
